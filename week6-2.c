d617367585d0c4ecb3310c386ccaf569662c1e93
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-21 19:15:22 +0800

    week6-2 finish

--- week6-2.c
+++ week6-2.c
@@ -1,20 +1,18 @@
-//
-// Created by ysche on 2022/3/21.
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int compare(const void *a,const void *b){
-    return (int*)b>=(int*)a;
+    return *(char*)a>*(char*)b;
 }
 int main(){
-    char s[1000000];
+    char s[1000000]={""};
+    printf("please enter a string=");
     fgets(s,sizeof (s),stdin);
     for (int i = 0; i < strlen(s); ++i) {
         if(s[i]>96)
             s[i]-=32;
     }
-    puts(s);
-    qsort(s, strlen(s),sizeof (s[0]),compare);
+    qsort(s, strlen(s)-1,sizeof (char),compare);
+    printf("after sorting=");
     puts(s);
 }
