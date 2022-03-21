d1078d71089d79361dca44833a490b7d500f6714
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-21 18:55:21 +0800

    week6-2

--- week6-2.c
+++ week6-2.c
@@ -1,4 +1,20 @@
 //
 // Created by ysche on 2022/3/21.
 //
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int compare(const void *a,const void *b){
+    return (int*)b>=(int*)a;
+}
+int main(){
+    char s[1000000];
+    fgets(s,sizeof (s),stdin);
+    for (int i = 0; i < strlen(s); ++i) {
+        if(s[i]>96)
+            s[i]-=32;
+    }
+    puts(s);
+    qsort(s, strlen(s),sizeof (s[0]),compare);
+    puts(s);
+}
