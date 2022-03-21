b1f8a11e0efe00ffaadb4ab6f306a651f11f40f6
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-03-21 18:19:27 +0800

    week6-1

--- week6-1.c
+++ week6-1.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+const char A_list[][3]={"  ","  ","A=","  ","  "};
+const char B_list[][3]={"  ","B=","  "};
+const char C_list[][3]={"  ","C=","  "};
 
+int ** func(const int A[5][5], const int B[3][3], int C[3][3], int row, int colum);
+
+void print_square5(int ** square);
+void print_square3(int ** square,char a) ;
 int main() {
-    printf("This is branch main");
+    const int A[][5]={{1,1,1,0,0},
+                      {0,1,1,1,0},
+                      {0,0,1,1,1},
+                      {0,0,1,1,0},
+                      {0,1,1,0,0}
+    };
+    const int B[][3]={{1,0,1},
+                      {0,1,0},
+                      {1,0,1}
+    };
+    int C[3][3]={0};
+    print_square5(A);
+    print_square3(B, 'B');
+    print_square3(C, 'C');
+    int **k=func(A,B,C,0,0);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            printf("%d",k[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
+}
+
+void print_square5(int ** square) {
+    for (int i = 0; i < 5; ++i) {
+        printf("%s",A_list[i]);
+        for (int j = 0; j < 5; ++j) {
+                printf("%d",square[i][j]);
+        }
+        printf("\n");
+    }
+}
+void print_square3(int ** square,char a) {
+    for (int i = 0; i < 3; ++i) {
+        if(a=='B')
+            printf("%s",B_list[i]);
+        else
+            printf("%s",C_list[i]);
+        for (int j = 0; j < 3; ++j) {
+            printf("%d",square[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int ** func(const int A[5][5], const int B[3][3], int C[3][3], int row, int colum) {
+    if(row==3 && colum==3)
+        return C;
+    for (int i = row; i < row+3; ++i) {
+        for (int j = colum; j < colum+3; ++j) {
+            C[i][j]+=A[i][j]*B[i][j];
+        }
+    }
+    if(colum==2)
+        func(A,B,C,row+1,0);
+    else
+        func(A,B,C,row,colum+1);
 }
