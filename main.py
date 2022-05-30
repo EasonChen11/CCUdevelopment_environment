df6bc2ae0832cbc6e6b3ce66b1e7d20ca110f866
EasonChen11 <99108285+EasonChen11@users.noreply.github.com>
2022-05-30 19:28:15 +0800

    finish

--- main.py
+++ main.py
@@ -1,43 +1,123 @@
+# -*- coding: utf-8 -*-
 import random
-ans_list = [4,5,4,7]
-check_list = {}
-# for i in range(4):
-#     r = random.randrange(0, 9)
-#     ans_list.append(r)
 
-print(f"answer is={ans_list}")
 
-endgame = False
-while not endgame:
-    AB = {'A': 0, 'B': 0}
-    input_list = []
-    inp = input(f"guss a number")
-    for a in inp:
-        input_list.append(int(a))
-    for i in range(len(input_list)):
-        print(f"{input_list[i]}", end=' ')
-    print("")
-    save = 10
-    for i in range(4):
-        if input_list[i] == ans_list[i]:
-            AB['A'] += 1
-            input_list[i] = save
-            save += 1
-            ans_list[i] = save
-            save += 1
-    for j in range(4):
-        for k in range(4):
-            if input_list[j] == ans_list[k]:
-                AB['B'] += 1
-                input_list[j] = save
-                save += 1
-                ans_list[k] = save
-                save += 1
-    for x, y in AB.items():
-        print(f"{y} {x}", end=' ')
-    print("")
-    if AB['A'] == 4:
-        endgame = True
-        print(f"You guess it right!")
-    else:
-        print(f"Try again!")
+class Boss:
+    def __init__(self):
+        self.name = "Boss"
+        self.HP = 500
+        self.skill = {1: ("招式一", 150), 2: ("招式二", 100), 3: ("招式三", 200), 4: ("招式四", 50)}
+
+    def attack(self, player):
+        boss_damage = self.skill[random.randrange(1, 5, 1)]
+        player_damaged = player[random.randrange(0, 2, 1)]
+        player_damaged.HP -= boss_damage[1]
+        if player_damaged.HP < 0:
+            player_damaged.HP = 0
+        print(f"{self.name}對{player_damaged.name}使出了{boss_damage[0]}造成了{boss_damage[1]}傷害")
+
+
+class P1:
+    def __init__(self, name):
+        self.name = name
+        self.HP = 300
+        self.attack = 100
+        self.step = 0
+
+    def update(self):
+        if self.step == 1:
+            self.Attack()
+        else:
+            self.heal()
+
+    def Attack(self):
+        global boss
+        boss.HP -= self.attack
+        if boss.HP < 0:
+            boss.HP = 0
+        print(f"{self.name:<10}對{boss.name}造成{self.attack}傷害")
+
+    def heal(self):
+        self.HP += 100
+        print(f"{self.name:<10}回復了些許生命")
+        if self.HP > 300:
+            self.HP = 300
+
+
+class P2:
+
+    def __init__(self, name):
+        self.name = name
+        self.HP = 250
+        self.attack = 150
+        self.step = 0
+
+    def update(self):
+        if self.step == 1:
+            self.Attack()
+        else:
+            self.heal()
+
+    def Attack(self):
+        global boss
+        boss.HP -= self.attack
+        if boss.HP < 0:
+            boss.HP = 0
+        print(f"{self.name:<10}對{boss.name}造成{self.attack}傷害")
+
+    def heal(self):
+        self.HP += 100
+        print(f"{self.name:<10}回復了些許生命")
+        if self.HP > 250:
+            self.HP = 250
+
+
+boss = Boss()
+
+name1 = input("p1's name:")
+name2 = input("p2's name:")
+player1 = P1(name1)
+player2 = P2(name2)
+print(f"Game start !\n{player1.name} and {player2.name} fight with Boss")
+
+
+def print_data():
+    print(f"{boss.name:<10}\t:HP:{boss.HP:<3} \tSkill:", end='')
+    for skill, attack in boss.skill.values():
+        print(f"{skill}({attack})", end='')
+    print()
+    print(f"{player1.name:<10} \t:HP:{player1.HP:<3} \tATK:{player1.attack:<3}")
+    print(f"{player2.name:<10} \t:HP:{player2.HP:<3} \tATK:{player2.attack:<3}")
+    print()
+
+
+def boss_round():
+    print("----------Boss的回合----------")
+    print_data()
+    boss.attack((player1, player2))
+
+
+def player_round():
+    print("\n----------你的回合----------")
+    print_data()
+    player1.step = int(input(f"{player1.name:<10}(1.attack 2.heal)"))
+    player2.step = int(input(f"{player2.name:<10}(1.attack 2.heal)"))
+    player1.update()
+    player2.update()
+    print()
+
+
+while True:
+    boss_round()
+    if player1.HP == 0 or player2.HP == 0:
+        break
+    player_round()
+    if boss.HP == 0:
+        break
+
+if boss.HP:
+    print_data()
+    print("You Lose")
+else:
+    print_data()
+    print("You Win")
